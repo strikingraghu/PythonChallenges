8e0a4d4e2892a7dd984751598e443cfe48ca1855
strikingraghu <strikingraghu@gmail.com>
2020-08-28 22:10:17 +0530

    Python-Challenges

--- Challenge_13.py
+++ Challenge_13.py
@@ -33,3 +33,31 @@ def finding_total():
 
 # calling function
 finding_total()
+
+
+"""
+Problem Description
+The program takes in an integer and prints the smallest divisor of the integer.
+
+Problem Solution
+1. Take in an integer from the user.
+2. Use a for loop where the value of i ranges from 2 to the integer.
+3. If the number is divisible by i, the value of i is appended to the list.
+4. The list is then sorted and the smallest element is printed.
+5. Exit.
+"""
+
+
+def identifying_smallest_divisor():
+    print()
+    incoming_values = int(input("Enter a number: "))
+    divisor_list = []
+    for each_val in range(2, int(incoming_values/2) + 1):
+        if incoming_values % each_val == 0:
+            divisor_list.append(each_val)
+    divisor_list.sort()
+    print("Least divisor for the provided number: ", divisor_list[0])
+
+
+# calling function
+identifying_smallest_divisor()
