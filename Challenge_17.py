b31be8f350d7fc1206bfc47ac6ff10acb9ab71df
strikingraghu <strikingraghu@gmail.com>
2020-11-03 12:41:21 +0530

    Python-Challenges

---
+++ Challenge_17.py
@@ -0,0 +1,26 @@
+"""
+This is a Python Program to reverse a given number.
+
+Problem Description
+The program takes a number and reverses it.
+
+Problem Solution
+1. Take the value of the integer and store in a variable.
+2. Using a while loop, get each digit of the number and store the reversed number in another variable.
+3. Print the reverse of the number.
+4. Exit.
+"""
+
+
+def reverse_number():
+    print()
+    user_input = int(input("Enter any number to get it reversed: "))
+    reversed_value = 0
+    while (user_input > 0):
+        dig = user_input % 10
+        reversed_value = reversed_value * 10 + dig
+        user_input = user_input//10
+    print("Provided number is now been reversed and it's value: ", reversed_value)
+
+
+reverse_number()
