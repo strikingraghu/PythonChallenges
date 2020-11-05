adb91a6d982df75d984ec914ebfe832d89ed31b0
strikingraghu <strikingraghu@gmail.com>
2020-11-05 23:07:51 +0530

    Python-Challenges

--- Challenge_20_Imp.py
+++ Challenge_20_Imp.py
@@ -11,6 +11,20 @@ Problem Solution
 4. Exit.
 """
 
+"""
+This is a Python Program to read a number n and print an identity matrix of the desired size.
+
+Problem Description
+The program takes a number n and prints an identity matrix of the desired size.
+
+Problem Solution
+1. Take a value from the user and store it in a variable n.
+2. Use two for loop where the value of j ranges between the values of 0 and n-1 
+3. Value of i also ranges between 0 and n-1.
+4. Print the value of 1 when i is equal to j and 0 otherwise.
+5. Exit.
+"""
+
 
 def fetch_possible_combinations():
     print()
@@ -25,4 +39,17 @@ def fetch_possible_combinations():
                     print(values_list[i], values_list[j], values_list[k])
 
 
-fetch_possible_combinations()
+def build_identity_matrix():
+    print()
+    user_input = int(input("Enter any number of your choice: "))
+    for i in range(0, user_input):
+        for j in range(0, user_input):
+            if i == j:
+                print("1", sep=" ", end=" ")
+            else:
+                print("0", sep=" ", end=" ")
+        print()
+
+
+# fetch_possible_combinations()
+build_identity_matrix()
