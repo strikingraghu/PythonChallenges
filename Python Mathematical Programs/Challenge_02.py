c50ca02f44f633b20e59fc21dfe7d69648d02c46
strikingraghu <strikingraghu@gmail.com>
2020-11-07 23:21:37 +0530

    Python-Challenges

--- Python Mathematical Programs/Challenge_02.py
+++ Python Mathematical Programs/Challenge_02.py
@@ -25,4 +25,33 @@ def calculate_interest():
         print("ValueError:: Please provide integers only!")
 
 
+"""
+This is a Python Program to check whether a given year is a leap year or not.
+
+Problem Description
+The program takes in a year and checks whether it is a leap year or not.
+
+Problem Solution
+1. Take the value of the year as input
+2. Using an if-statement, check whether the year is a leap year or not
+3. Print the final result
+4. Exit
+"""
+
+
+def validate_leap_year():
+    print()
+
+    try:
+        year = int(input("Enter a specific year of your choice: "))
+        if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+            print("Wow, it's a leap year!")
+        else:
+            print("Entered year is not a leap year, try again!")
+    except ValueError as ve:
+        print(ve.__traceback__)
+        print("ValueError: Please provide integers value only!")
+
+
 calculate_interest()
+validate_leap_year()
