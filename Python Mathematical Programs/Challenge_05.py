483c230762456c2ee820d6da80e33b2c096e574c
strikingraghu <strikingraghu@gmail.com>
2020-11-12 22:26:05 +0530

    Python-Challenges

--- Python Mathematical Programs/Challenge_05.py
+++ Python Mathematical Programs/Challenge_05.py
@@ -22,3 +22,33 @@ def create_tables_output():
 
 
 create_tables_output()
+
+"""
+This is a Python Program to print the sum of negative numbers, positive even numbers and positive odd numbers in a given list.
+
+Problem Description
+The program prints the sum of negative numbers, positive even numbers and positive odd numbers in a given list..
+
+Problem Solution
+1. Take in the number of elements to be in the list from the user.
+2. Take in the elements from the user using a for loop and append to a list.
+3. Using a for loop, get the elements one by one from the list and check if it is positive or negative.
+4. If it is positive, check if it is odd or even and find the individual sum.
+5. Find the individual sum of negative numbers.
+6. Print all the sums.
+7. Exit.
+"""
+
+
+def calculate_various_pattern_numbers():
+    print()
+    try:
+        input_loop_val = int(input("Enter the length of numbers you want to provide: "))
+        setting_range = range(1, input_loop_val+1)
+        for each_get_val in setting_range:
+            print(each_get_val)
+    except ValueError as ve:
+        pass
+
+
+calculate_various_pattern_numbers()
