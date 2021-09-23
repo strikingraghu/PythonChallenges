8e20267ce6913c43561b73f01f0f3d7a31a0f640
strikingraghu <strikingraghu@gmail.com>
2021-09-23 12:01:56 +0530

    Pyhon-Challenges

--- Python Mathematical Programs/Challenge_08.py
+++ Python Mathematical Programs/Challenge_08.py
@@ -14,27 +14,36 @@ Problem Solution
 
 def check_amicable_num_validation():
 
-    x = int(input("Enter the first number: "))
-    y = int(input("Enter the second number: "))
-    print(x, y)
-    sum1 = 0
-    sum2 = 0
-    for i in range(1,x):
-        if x % i == 0:
-            print(i)
-            sum1 += i
-    print("Closing execution for x value")
-    
-    for j in range(1,y):
-        if y % j == 0:
-            print(j)
-            sum2 += j
-    print("Closing the execution for y value")
 
-    if sum1 == y and sum2 == x:
-        print("Above provided numbers are amicable")
-    else:
-        print("Above provided numbers are not amicable!")
+    try:
+        x = int(input("Enter the first number: "))
+        y = int(input("Enter the second number: "))
+
+        if type(x or y) == int:
+            print("Provided input values are part of integer category")
+
+            sum1 = 0
+            sum2 = 0
+            for i in range(1,x):
+                if x % i == 0:
+                    print(i)
+                    sum1 += i
+            print("Closing execution for x value")
+
+            for j in range(1,y):
+                if y % j == 0:
+                    print(j)
+                    sum2 += j
+            print("Closing the execution for y value")
+
+            if sum1 == y and sum2 == x:
+                print("Above provided numbers are amicable")
+            else:
+                print("Above provided numbers are not amicable!")
+                
+    except (BaseException, UnboundLocalError, ValueError) as e:
+        print("Logging error: ", e)
+        print("Please check the provided input values!")
 
 
 check_amicable_num_validation()
