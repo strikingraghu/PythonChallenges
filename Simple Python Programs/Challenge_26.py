cde98fbead62e56469f6852d556cca7c30ef9b02
Rathnamachary <raghavendra.rathnamachary@emc.com>
2022-04-11 10:54:56 +0800

    New challenges are added into the repository

---
+++ Simple Python Programs/Challenge_26.py
@@ -0,0 +1,9 @@
+# When input() function executes program flow will be stopped until the user has given an input.
+# The text or message display on the output screen to ask a user to enter input value is optional.
+# Which means, the prompt, will be printed on the screen is optional.
+# Whatever you enter as input, input function convert it into a string.
+# If you enter an integer value still input() function convert it into a string.
+# You need to explicitly convert it into an integer in your code using typecasting.
+
+num = input("Enter the number of your choice: ")
+print("Number provided by you is: ", num)
