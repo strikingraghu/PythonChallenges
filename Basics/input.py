f283ed5605f43344aea1b2599d02f982eae9ee63
strikingraghu <strikingraghu@gmail.com>
2026-01-19 14:50:23 +0800

    Python basics

--- Basics/input.py
+++ Basics/input.py
@@ -2,3 +2,14 @@
 
 name = input("Enter your name: ")
 print("Hello, have a great day, " + name + "!")
+
+# Example of formatted string (f-string)
+person = "Angelina"
+age = 25
+city = "Bengaluru"
+print(f"{person} is {age} years old and lives in {city}.")
+
+# Taking mulple inputs in a single line
+name, age = input("Enter your name and age: ").split()
+print("Entered Name: ", name)
+print("Entered Age: ", age)
